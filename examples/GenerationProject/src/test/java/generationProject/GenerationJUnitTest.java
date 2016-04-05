7a191f552ff4393f3ffafc63c2e0378face96802
TobiasBieschke <tobias.bieschke@rwth-aachen.de>
2016-04-05 17:19:18 +0200

    Just a little Maven test-project

---
+++ examples/GenerationProject/src/test/java/generationProject/GenerationJUnitTest.java
@@ -0,0 +1,41 @@
+package generationProject;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class GenerationJUnitTest 
+    extends TestCase
+{
+	/**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public GenerationJUnitTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( GenerationJUnitTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    
+    public void testParser()
+    {
+    	String [] args = {"src/test/resources/HTMLRed.mc4"};
+    	GenerationTest.run(args);
+    	assertTrue(true);
+    }
+}
